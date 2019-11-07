5369bf061426d23f3651211129661ac00b9c2ecf
Joe Summerhayes <joesummerhayes09@hotmail.com>
2019-11-07 13:12:15 +0000

    google form and image tweak

--- src/Card.js
+++ src/Card.js
@@ -10,7 +10,7 @@ class Card extends React.Component {
             <div>
                 <div className="center-card">
                     <div className="logo">
-                        <img src={logo} />
+                        <img src={logo} alt="logo"/>
                     </div>
                     <div>
                         <p className="wedding-date">MAY 30TH 2020</p>
@@ -18,12 +18,16 @@ class Card extends React.Component {
                     <div className="countdown-section">
                         <Countdown />
                     </div>
-                    <div className="rsvp-button">
-                        <button>RSVP HERE</button>
-                    </div>
+                    <a href="https://docs.google.com/forms/d/e/1FAIpQLSfXHfuhhD7ydCiXGziPF34puX2vwVLAuU2L9zq0xquY6jA-lA/viewform" target="_blank">
+                        <div className="rsvp-button">
+                            <button>
+                                RSVP HERE
+                            </button>
+                        </div>
+                    </a>
                 </div>
                 <div className="adam-image">
-                    <img src={picture} />
+                    <img src={picture} alt="proposal"/>
                 </div>
             </div>
         )
